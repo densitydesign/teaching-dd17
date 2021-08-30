160b242f7a7874d08ccc21d57e662b94aaa25731
bndndr <benedetti.a93@gmail.com>
2021-08-30 11:47:42 +0200

    init

---
+++ gatsby-config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "teaching-dd17",
+  },
+  plugins: [
+    "gatsby-plugin-sass",
+    "gatsby-plugin-image",
+    // {
+    //   resolve: "gatsby-plugin-google-analytics",
+    //   options: {
+    //     trackingId: "",
+    //   },
+    // },
+    "gatsby-plugin-sitemap",
+    "gatsby-plugin-mdx",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+  ],
+};
---
+++ src/components/navbar.js
@@ -0,0 +1,17 @@
+import React from "react"
+import { StaticQuery, graphql } from "gatsby"
+export default function Navbar() {
+    return (
+        <nav>
+            <h1>DD22</h1>
+            <ul>
+                <li>
+                    Starter pack
+                </li>
+                <li>
+                    Course results
+                </li>
+            </ul>
+        </nav>
+    )
+}
