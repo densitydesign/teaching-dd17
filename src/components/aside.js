2df86b82f0695e1cdbd98e50bfd24e1ba4389dbd
andrea <benedetti.a93@gmail.com>
2021-09-13 11:17:41 +0200

    Update aside.js

--- src/components/aside.js
+++ src/components/aside.js
@@ -12,6 +12,12 @@ export default function Aside() {
                 <li>
                     <a href="https://www4.ceda.polimi.it/manifesti/manifesti/controller/ManifestoPublic.do?EVN_DETTAGLIO_RIGA_MANIFESTO=evento&aa=2021&k_cf=19&k_corso_la=1162&k_indir=***&codDescr=052799&lang=EN&semestre=1&anno_corso=2&idItemOfferta=154784&idRiga=267427" target="_blank">2021/2022 Course manifesto</a>
                 </li>
+                <li>
+                    <a href="https://t.me/density17" target="_blank">Telegram Channel</a>
+                </li>
+                <li>
+                    <a href="https://drive.google.com/drive/folders/1UUxOIWTZFKAYVjlrfpAzPCEJayv8fcxY?usp=sharing" target="_blank">Google Drive Folder</a>
+                </li>
             </ul>
         </aside>
     )
